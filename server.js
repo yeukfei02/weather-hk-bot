e8b8ae8d1ca46a81d478119caf6af962b2c8f9c6
Donald Wu <yeukfei02@gmail.com>
2019-12-21 02:22:21 +0800

    fix code and command

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log('OK. The bot is running...')
+console.log('OK. The bot is running...');
 
 // bot name: weatherHKBot
 // link: t.me/weatherHKBot
@@ -15,27 +15,27 @@ const htmlToText = require('html-to-text');
 
 // store user setting
 let languageObj = {
-    english: 'English',
-    traditionalChinese: '繁體中文',
-    simplifiedChinese: '简体中文'
+  english: 'English',
+  traditionalChinese: '繁體中文',
+  simplifiedChinese: '简体中文'
 };
 
 let currentURLObj = {
-    english: 'http://rss.weather.gov.hk/rss/CurrentWeather.xml',
-    traditionalChinese: 'http://rss.weather.gov.hk/rss/CurrentWeather_uc.xml',
-    simplifiedChinese: 'http://gbrss.weather.gov.hk/rss/CurrentWeather_uc.xml'
+  english: 'http://rss.weather.gov.hk/rss/CurrentWeather.xml',
+  traditionalChinese: 'http://rss.weather.gov.hk/rss/CurrentWeather_uc.xml',
+  simplifiedChinese: 'http://gbrss.weather.gov.hk/rss/CurrentWeather_uc.xml'
 };
 
 let warningInformationURLObj = {
-    english: 'http://rss.weather.gov.hk/rss/WeatherWarningBulletin.xml',
-    traditionalChinese: 'http://rss.weather.gov.hk/rss/WeatherWarningBulletin_uc.xml',
-    simplifiedChinese: 'http://gbrss.weather.gov.hk/rss/WeatherWarningBulletin_uc.xml'
+  english: 'http://rss.weather.gov.hk/rss/WeatherWarningBulletin.xml',
+  traditionalChinese: 'http://rss.weather.gov.hk/rss/WeatherWarningBulletin_uc.xml',
+  simplifiedChinese: 'http://gbrss.weather.gov.hk/rss/WeatherWarningBulletin_uc.xml'
 };
 
 let warningSummaryURLObj = {
-    english: 'http://rss.weather.gov.hk/rss/WeatherWarningSummaryv2.xml',
-    traditionalChinese: 'http://rss.weather.gov.hk/rss/WeatherWarningSummaryv2_uc.xml',
-    simplifiedChinese: 'http://gbrss.weather.gov.hk/rss/WeatherWarningSummaryv2_uc.xml'
+  english: 'http://rss.weather.gov.hk/rss/WeatherWarningSummaryv2.xml',
+  traditionalChinese: 'http://rss.weather.gov.hk/rss/WeatherWarningSummaryv2_uc.xml',
+  simplifiedChinese: 'http://gbrss.weather.gov.hk/rss/WeatherWarningSummaryv2_uc.xml'
 };
 
 let subscribeWarning = true;
@@ -45,32 +45,32 @@ let warningInformationURL = warningInformationURLObj.english;
 let warningSummaryURL = warningSummaryURLObj.english;
 
 function getCurrentResponse($, urlLink, strToShow) {
-	// current response
-	request({
+  // current response
+  request({
     url: urlLink,
     json: true
-	}, (error, response, body) => {
+  }, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       parseString(body, (err, result) => {
-		    for (let i = 0; i < result.rss.channel.length; i++) {
-		    	for (let j = 0; j < result.rss.channel[i].item.length; j++) {
-		    		let currentResponse = result.rss.channel[i].item[j].description.toString();
+        for (let i = 0; i < result.rss.channel.length; i++) {
+          for (let j = 0; j < result.rss.channel[i].item.length; j++) {
+            let currentResponse = result.rss.channel[i].item[j].description.toString();
 
-		    		let text = htmlToText.fromString(currentResponse, {
-							wordwrap: 130,
-							//ignoreImage: true
-					  });
-			    	$.sendMessage('------------------ [' + strToShow + '] ------------------');
-						$.sendMessage(text);
-		    	}
-		    }
-	    });
+            let text = htmlToText.fromString(currentResponse, {
+              wordwrap: 130,
+              //ignoreImage: true
+            });
+            $.sendMessage(`------------------ [${strToShow}] ------------------`);
+            $.sendMessage(text);
+          }
+        }
+      });
     }
-	});
+  });
 }
 
 function getWarningResponse($, urlLink, strToShow) {
-	// warning response
+  // warning response
   request({
     url: urlLink,
     json: true
@@ -81,9 +81,9 @@ function getWarningResponse($, urlLink, strToShow) {
           for (let j = 0; j < result.rss.channel[i].item.length; j++) {
             let warningResponse = result.rss.channel[i].item[j].description.toString();
             let text = htmlToText.fromString(warningResponse, {
-                wordwrap: 130
+              wordwrap: 130
             });
-            $.sendMessage('------------------ [' + strToShow + '] ------------------');
+            $.sendMessage(`------------------ [${strToShow}] ------------------`);
             $.sendMessage(text);
           }
         }
@@ -92,185 +92,226 @@ function getWarningResponse($, urlLink, strToShow) {
   });
 }
 
-class TopicsController extends TelegramBaseController {
-    topicsHandler($) {
-  		switch (language) {
-  			case 'English':
-  				getCurrentResponse($, currentURL, 'Current');
-  				if (subscribeWarning) {
-  					getWarningResponse($, warningInformationURL, 'Warning');
-  				} else {
-  					$.sendMessage('Please subscribe warning');
-  				}
-  				break;
-  			case '繁體中文':
-  				getCurrentResponse($, currentURL, '現時');
-  				if (subscribeWarning) {
-  					getWarningResponse($, warningInformationURL, '警告');
-  				} else {
-  					$.sendMessage('Please subscribe warning');
-  				}
-  				break;
-  			case '简体中文':
-  				getCurrentResponse($, currentURL, '现时');
-  				if (subscribeWarning) {
-  					getWarningResponse($, warningInformationURL, '警告');
-  				} else {
-  					$.sendMessage('Please subscribe warning');
-  				}
-  				break;
-    		}
-    }
+class StartController extends TelegramBaseController {
+  startHandler($) {
+    $.sendMessage(`
+      ### Example command ###
+/start
+Show all example command
 
-    get routes() {
-      return {
-        'topicsCommand': 'topicsHandler'
-      }
+/tellMeCurrentAndWarning
+List out the topic of current weather, warning
+
+/tellMeCurrent
+Echo back the current info in forecast feed
+
+/tellMeWarning
+Echo back the current info in weather warning
+
+/subscribeWarning
+Enable warning message
+
+/unsubscribeWarning
+Disable warning message
+
+/繁體中文
+Set content in Traditional Chinese
+
+/简体中文
+Set content in Simplified Chinese
+
+/english
+Set content in English
+    `);
+  }
+
+  get routes() {
+    return {
+      'startCommand': 'startHandler'
     }
+  }
+}
+
+class TellMeCurrentAndWarningController extends TelegramBaseController {
+  tellMeCurrentAndWarningHandler($) {
+    switch (language) {
+      case 'English':
+        getCurrentResponse($, currentURL, 'Current');
+        if (subscribeWarning) {
+          getWarningResponse($, warningInformationURL, 'Warning');
+        } else {
+          $.sendMessage('Please subscribe warning');
+        }
+        break;
+      case '繁體中文':
+        getCurrentResponse($, currentURL, '現時');
+        if (subscribeWarning) {
+          getWarningResponse($, warningInformationURL, '警告');
+        } else {
+          $.sendMessage('Please subscribe warning');
+        }
+        break;
+      case '简体中文':
+        getCurrentResponse($, currentURL, '现时');
+        if (subscribeWarning) {
+          getWarningResponse($, warningInformationURL, '警告');
+        } else {
+          $.sendMessage('Please subscribe warning');
+        }
+        break;
+    }
+  }
+
+  get routes() {
+    return {
+      'tellMeCurrentAndWarningCommand': 'tellMeCurrentAndWarningHandler'
+    }
+  }
 }
 
 class TellmeCurrentController extends TelegramBaseController {
-    tellMeCurrentHandler($) {
-    	switch (language) {
-  			case 'English':
-  				getCurrentResponse($, currentURL, 'Current');
-  				break;
-  			case '繁體中文':
-  				getCurrentResponse($, currentURL, '現時');
-  				break;
-  			case '简体中文':
-  				getCurrentResponse($, currentURL, '现时');
-  				break;
-      }
+  tellMeCurrentHandler($) {
+    switch (language) {
+      case 'English':
+        getCurrentResponse($, currentURL, 'Current');
+        break;
+      case '繁體中文':
+        getCurrentResponse($, currentURL, '現時');
+        break;
+      case '简体中文':
+        getCurrentResponse($, currentURL, '现时');
+        break;
     }
+  }
 
-    get routes() {
-      return {
-        'tellMeCurrentCommand': 'tellMeCurrentHandler'
-      }
+  get routes() {
+    return {
+      'tellMeCurrentCommand': 'tellMeCurrentHandler'
     }
+  }
 }
 
 class TellmeWarningController extends TelegramBaseController {
-	 tellMeWarningHandler($) {
-  		switch (language) {
-  			case 'English':
-  				if (subscribeWarning) {
-  					getWarningResponse($, warningSummaryURL, 'Warning');
-  				} else {
-  					$.sendMessage('Please subscribe warning');
-  				}
-  				break;
-  			case '繁體中文':
-  				if (subscribeWarning) {
-  					getWarningResponse($, warningSummaryURL, '警告');
-  				} else {
-  					$.sendMessage('Please subscribe warning');
-  				}
-  				break;
-  			case '简体中文':
-  				if (subscribeWarning) {
-  					getWarningResponse($, warningSummaryURL, '警告');
-  				} else {
-  					$.sendMessage('Please subscribe warning');
-  				}
-  				break;
-  		 }
+  tellMeWarningHandler($) {
+    switch (language) {
+      case 'English':
+        if (subscribeWarning) {
+          getWarningResponse($, warningSummaryURL, 'Warning');
+        } else {
+          $.sendMessage('Please subscribe warning');
+        }
+        break;
+      case '繁體中文':
+        if (subscribeWarning) {
+          getWarningResponse($, warningSummaryURL, '警告');
+        } else {
+          $.sendMessage('Please subscribe warning');
+        }
+        break;
+      case '简体中文':
+        if (subscribeWarning) {
+          getWarningResponse($, warningSummaryURL, '警告');
+        } else {
+          $.sendMessage('Please subscribe warning');
+        }
+        break;
     }
+  }
 
-    get routes() {
-      return {
-        'tellMeWarningCommand': 'tellMeWarningHandler'
-      }
+  get routes() {
+    return {
+      'tellMeWarningCommand': 'tellMeWarningHandler'
     }
+  }
 }
 
 class SubscribeWarningController extends TelegramBaseController {
-  	subscribeWarningHandler($) {
-    	subscribeWarning = true;
+  subscribeWarningHandler($) {
+    subscribeWarning = true;
 
-      $.sendMessage('You just subscribe warning');
-    }
+    $.sendMessage('You just subscribe warning');
+  }
 
-  	get routes() {
-      return {
-        'subscribeWarningCommand': 'subscribeWarningHandler'
-      }
+  get routes() {
+    return {
+      'subscribeWarningCommand': 'subscribeWarningHandler'
     }
+  }
 }
 
 class UnsubscribeWarningController extends TelegramBaseController {
-  	unsubscribeWarningHandler($) {
-    	subscribeWarning = false;
+  unsubscribeWarningHandler($) {
+    subscribeWarning = false;
 
-      $.sendMessage('You just unsubscribe warning');
-    }
+    $.sendMessage('You just unsubscribe warning');
+  }
 
-  	get routes() {
-      return {
-        'unsubscribeWarningCommand': 'unsubscribeWarningHandler'
-      }
+  get routes() {
+    return {
+      'unsubscribeWarningCommand': 'unsubscribeWarningHandler'
     }
+  }
 }
 
 class EnglishController extends TelegramBaseController {
-  	englishHandler($) {
-  		language = languageObj.english;
-  		currentURL = currentURLObj.english;
-  		warningInformationURL = warningInformationURLObj.english;
-  		warningSummaryURL = warningSummaryURLObj.english;
+  englishHandler($) {
+    language = languageObj.english;
+    currentURL = currentURLObj.english;
+    warningInformationURL = warningInformationURLObj.english;
+    warningSummaryURL = warningSummaryURLObj.english;
 
-      $.sendMessage('OK');
-    }
+    $.sendMessage('OK');
+  }
 
-	  get routes() {
-      return {
-        'englishCommand': 'englishHandler'
-      }
+  get routes() {
+    return {
+      'englishCommand': 'englishHandler'
     }
+  }
 }
 
 
 class TraditionalChineseController extends TelegramBaseController {
-	  traditionalChineseHandler($) {
-  		language = languageObj.traditionalChinese;
-  		currentURL = currentURLObj.traditionalChinese;
-  		warningInformationURL = warningInformationURLObj.traditionalChinese;
-  		warningSummaryURL = warningSummaryURLObj.traditionalChinese;
+  traditionalChineseHandler($) {
+    language = languageObj.traditionalChinese;
+    currentURL = currentURLObj.traditionalChinese;
+    warningInformationURL = warningInformationURLObj.traditionalChinese;
+    warningSummaryURL = warningSummaryURLObj.traditionalChinese;
 
-      $.sendMessage('知道了');
-    }
+    $.sendMessage('知道了');
+  }
 
-	  get routes() {
-      return {
-        'traditionalChineseCommand': 'traditionalChineseHandler'
-      }
+  get routes() {
+    return {
+      'traditionalChineseCommand': 'traditionalChineseHandler'
     }
+  }
 }
 
 class SimplifiedChineseController extends TelegramBaseController {
-    simplifiedChineseHandler($) {
-    	language = languageObj.simplifiedChinese;
-  		currentURL = currentURLObj.simplifiedChinese;
-  		warningInformationURL = warningInformationURLObj.simplifiedChinese;
-  		warningSummaryURL = warningSummaryURLObj.simplifiedChinese;
+  simplifiedChineseHandler($) {
+    language = languageObj.simplifiedChinese;
+    currentURL = currentURLObj.simplifiedChinese;
+    warningInformationURL = warningInformationURLObj.simplifiedChinese;
+    warningSummaryURL = warningSummaryURLObj.simplifiedChinese;
 
-      $.sendMessage('知道了');
-    }
+    $.sendMessage('知道了');
+  }
 
-    get routes() {
-      return {
-        'simplifiedChineseCommand': 'simplifiedChineseHandler'
-      }
+  get routes() {
+    return {
+      'simplifiedChineseCommand': 'simplifiedChineseHandler'
     }
+  }
 }
 
 $.router
-	.when(new TextCommand('@Bot topics', 'topicsCommand'), new TopicsController())
-	.when(new TextCommand('@Bot tellme current', 'tellMeCurrentCommand'), new TellmeCurrentController())
-	.when(new TextCommand('@Bot tellme warning', 'tellMeWarningCommand'), new TellmeWarningController())
-	.when(new TextCommand('@Bot subscribe warning', 'subscribeWarningCommand'), new SubscribeWarningController())
-	.when(new TextCommand('@Bot unsubscribe warning', 'unsubscribeWarningCommand'), new UnsubscribeWarningController())
-	.when(new TextCommand('@Bot English', 'englishCommand'), new EnglishController())
-	.when(new TextCommand('@Bot 繁體中文', 'traditionalChineseCommand'), new TraditionalChineseController())
-	.when(new TextCommand('@Bot 简体中文', 'simplifiedChineseCommand'), new SimplifiedChineseController());
+  .when(new TextCommand('/start', 'startCommand'), new StartController())
+  .when(new TextCommand('/tellMeCurrentAndWarning', 'tellMeCurrentAndWarningCommand'), new TellMeCurrentAndWarningController())
+  .when(new TextCommand('/tellMeCurrent', 'tellMeCurrentCommand'), new TellmeCurrentController())
+  .when(new TextCommand('/tellMeWarning', 'tellMeWarningCommand'), new TellmeWarningController())
+  .when(new TextCommand('/subscribeWarning', 'subscribeWarningCommand'), new SubscribeWarningController())
+  .when(new TextCommand('/unsubscribeWarning', 'unsubscribeWarningCommand'), new UnsubscribeWarningController())
+  .when(new TextCommand('/english', 'englishCommand'), new EnglishController())
+  .when(new TextCommand('/繁體中文', 'traditionalChineseCommand'), new TraditionalChineseController())
+  .when(new TextCommand('/简体中文', 'simplifiedChineseCommand'), new SimplifiedChineseController());
